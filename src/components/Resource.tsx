d19688503fa00635295544fb31ee60c7b0611017
Aaryan Pashine <pashineaaryan@gmail.com>
2023-05-29 19:16:38 +0530

    need to add a favicon and a little fine tuning

--- src/components/Resource.tsx
+++ src/components/Resource.tsx
@@ -15,37 +15,55 @@ export const Resource: Component = () => {
 
   return (
     <>
-      <h1 class="header">Resources</h1>
-
       <div class="resources-container">
         <Suspense>
-          <For each={data()?.contents}>
-            {(block) => {
-              return (
-                <>
-                  <Space d={{ w: "0", h: "10px" }} />
-                </>
-              );
-            }}
-          </For>
-          <For each={data()?.contents}>
-            {(block) => {
-              return (
-                <>
-                  {block.class === "Link" ? (
-                    <div class="link">
-                      <h2>{block.title}</h2>
-                      <img src={block.image.display.url}></img>
-                    </div>
-                  ) : (
-                    <></>
-                  )}
-                  <Space d={{ w: "0", h: "10px" }} />
-                </>
-              );
-            }}
-          </For>
+          <h1>Channels</h1>
+          <div class="block-container">
+            <For each={data()?.contents}>
+              {(block) => {
+                return (
+                  <>
+                    {block.class === "Channel" ? (
+                      <a href={`https://are.na/channel/${block.slug}`}>
+                        <div class="channel">
+                          <h2>{block.title}</h2>
+                        </div>
+                      </a>
+                    ) : (
+                      <></>
+                    )}
+                    <Space d={{ w: "0", h: "10px" }} />
+                  </>
+                );
+              }}
+            </For>
+          </div>
+          <h1>Links</h1>
+          <div class="block-container">
+            <For each={data()?.contents}>
+              {(block) => {
+                return (
+                  <>
+                    {block.class === "Link" ? (
+                      <a href={block.source.url}>
+                        <div class="link">
+                          <h2>{block.title}</h2>
+                          <img src={block.image.display.url}></img>
+                        </div>
+                      </a>
+                    ) : (
+                      <></>
+                    )}
+                    <Space d={{ w: "0", h: "10px" }} />
+                  </>
+                );
+              }}
+            </For>
+          </div>
         </Suspense>
+        <a href="https://are.na/channel/api-playground-resources">
+          <p>Add to this channel</p>
+        </a>
       </div>
     </>
   );
